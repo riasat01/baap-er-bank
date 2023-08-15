541e3a899a85617aff01cd172a8343dd471e8302
riasat01 <riasat01@outlook.com>
2023-08-15 23:17:51 +0600

    bank inside added

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ document.querySelector('#btn-submit').addEventListener('click', function(){
     const email = document.querySelector('#user-email').value;
     const password = document.querySelector('#user-password').value;
     if(email === 'baaper@taka.com'  && password === 'takaEtaka'){
-        console.log("Taka r Poisha");
+        window.location.href = 'bank.html'
     }else{
-        console.log('Muri Kha');
+        alert('Muri Kha');
     }
 });
